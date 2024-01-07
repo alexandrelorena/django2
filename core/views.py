7af48cae3b62982a66af99e641adc435784e30cd
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-01-07 15:46:04 -0300

    Projeto finalizado

---
+++ core/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContatoForm, ProdutoModelForm
+from .models import Produto
+
+def index(request):
+    context = {
+        'produtos': Produto.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def contato(request):
+    form = ContatoForm(request.POST or None)
+    if str(request.method) == 'POST':
+        print(f'Post: {request.POST}')
+        if form.is_valid():
+            form.send_email()
+            messages.success(request, 'E-mail enviado com sucesso!')
+            form = ContatoForm()
+        else:
+            messages.error(request, 'Erro ao enviar e-mail')
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
+
+def produto(request):
+    if str(request.user) != 'AnonymousUser':
+        if str(request.method) == 'POST':
+            form = ProdutoModelForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Produto salvo com sucesso!')
+                form = ProdutoModelForm()
+            else:
+                messages.error(request, 'Erro ao salvar o produto')
+        else:
+            form = ProdutoModelForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'produto.html', context)
+    else:
+        return redirect('index')
+
+
